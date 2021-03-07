d97b5473187de1f69962553278badafe55d5dd78
stakingpoolspace <80047487+stakingpoolspace@users.noreply.github.com>
2021-03-07 15:43:53 +0100

    Added vbnt to available tokens
    You can now also check the vBNT pool space

--- bancorPool.js
+++ bancorPool.js
@@ -1,5 +1,6 @@
 const bancorPool = {
     'eth' : '0xb1CD6e4153B2a390Cf00A6556b0fC1458C4A5533',
     'link' : '0x04D0231162b4784b706908c787CE32bD075db9b7',
+    'vbnt' : '0x3D9E2dA44Af9386484d0D35C29eB62122e4F4742',
 }
 module.exports = { bancorPool };
